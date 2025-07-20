5f782307b8c33b3172c086dae8a78773da005b4a
aajuvale <ahmed.juvale@gmail.com>
2025-07-20 16:01:32 -0700

    feat: implemented buttons with roles

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green]), center: .center)
+        VStack {
+            Button("Button 1") {}
+                .buttonStyle(.bordered)
+            Button("Button 2", role: .destructive) {}
+                .buttonStyle(.bordered)
+            Button("Button 3") {}
+                .buttonStyle(.borderedProminent)
+                .tint(.mint )
+            Button("Button 4", role: .destructive) {}
+                .buttonStyle(.borderedProminent)
+        }
+    }
+    
+    func executeDelete() {
+        print("Now deleting")
     }
 }
 
